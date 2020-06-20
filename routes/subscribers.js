a8948551e878f73f3993a1e4213937f5bf3282c2
Leon <leonbaraza@gmail.com>
2020-06-20 13:39:02 +0300

    Configuring routes

---
+++ routes/subscribers.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const router = express.Router();
+
+// Getting all subscribers
+
+// Getting one subscriber
+
+// Creating one subscriber
+
+// Updating one subscriber
+
+// Deleting one subscriber
+
+
+module.exports = router
--- server.js
+++ server.js
@@ -1,15 +1,23 @@
+require('dotenv').config()
+
 const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
 
 
-mongoose.connect('mongodb://localhost/subscribers', { 
+mongoose.connect(process.env.DATABASE_URL, { 
     useNewUrlParser: true,
     useUnifiedTopology: true
  });
 const db = mongoose.connection
 db.on('error', (error) => console.error(error));
 db.once('open', () => console.log('Connected to database'));
+
+app.use(express.json());
+
+// configure routes
+const subscribersRoutes = require('./routes/subscribers');
+app.use('/subscribers',subscribersRoutes);
 
 
 // Define the port and listen

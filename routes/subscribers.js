b13e9bd13fc9bfe38bd5486b0387b3d8a4f776bd
Leon <leonbaraza@gmail.com>
2020-06-20 13:52:39 +0300

    Set up all routes

--- routes/subscribers.js
+++ routes/subscribers.js
@@ -2,14 +2,28 @@ const express = require('express');
 const router = express.Router();
 
 // Getting all subscribers
+router.get('/', (req, res) => {
+    res.send('Hello tests')
+});
 
 // Getting one subscriber
+router.get('/:id', (req, res) => {
+    
+});
 
 // Creating one subscriber
+router.post('/', (req, res) => {
+    
+});
 
 // Updating one subscriber
+router.patch('/:id', (req, res) => {
+    
+});
 
 // Deleting one subscriber
-
+router.delete('/:id', (req, res) => {
+    
+});
 
 module.exports = router
